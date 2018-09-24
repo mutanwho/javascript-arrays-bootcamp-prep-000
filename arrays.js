2712f4d345d0827405a363b7fa018dc684a9f749
Michelle M <mutanwho@gmail.com>
2018-09-24 20:03:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function destructivelyRemoveElementFromEndOfArray(array)
 return array};
   
 function removeElementFromEndOfArray(array)
-{return array.slice(-1)};
+{return array.slice(-2)};
   
   
   
